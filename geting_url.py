8debd2d6592fd97b1cb74f133d1e95af8fa2cd32
aymen <aymenfkir23@gmail.com>
2023-03-12 17:37:12 +0100

    Add files via upload

---
+++ geting_url.py
@@ -0,0 +1,34 @@
+# we need this library to make requets
+import requests
+
+# this library allow you to read html files and xml files
+from bs4 import BeautifulSoup
+
+
+# specify the url that you want to extract from it
+url = "https://en.wikipedia.org/wiki/Blog"
+# make a get request 
+reqs = requests.get(url)
+
+# this part is to create the to save in you can just created by your self
+try:
+    f = open("urls.txt", "x")
+except FileExistsError:
+    print("The file already exists.")
+# open a file with the argument w so you can write in it
+f = open("urls.txt","w")
+# reading the html file
+soup = BeautifulSoup(reqs.text,'html.parser')
+
+urls =[]
+# a loop so you can save all the extracted texts
+for link in soup.find_all('a'):
+    urls.append(link.get('href'))
+# save only the links in a file 
+for i in urls:
+    if i != None:
+        if i[0:5]=="https":
+            i+='\n'
+            f.write(i)
+# clear this list 
+urls.clear
